a7bb10f2ee5bb9706a5ee2a95bec426ab8edef6a
Gleb Zakharov <snyssfx@gmail.com>
2023-01-29 12:39:01 +0100

    rename: go-if-err-nil -> go-prettify-mode for more features

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# `go-if-err-nil.el`
-It is a minor mode for Emacs that replaces `if err != nil` statements in Go code to make them shorter and more informative using overlays.
-
-The mode converts popular words that is used in handling errors to just Greek letters or symbols.  
+# `go-prettify-mode.el`
+It is a minor mode for Emacs that replaces several Go statements, e.g. `if err != nil`, blocks with 1 code and `range` statement to make them shorter and more informative using overlays.
 
 __Before:__
 
@@ -17,22 +15,26 @@ Twice shorter!
 To turn it on in every Go buffer, add a hook:
 
 ``` emacs-lisp
-(add-hook 'go-mode-hook '(lambda () (go-if-err-nil-mode 1)))
+(add-hook 'go-mode-hook '(lambda () (go-prettify-mode 1)))
 ```
 
 To toggle it via a hotkey add this code:
 
 ``` emacs-lisp
-(define-key go-mode-map (kbd \"C-c C-e\") #'go-if-err-nil-mode)
+(define-key go-mode-map (kbd \"C-c C-e\") #'go-prettify-mode)
 ```
 
-You can customize a face for overlays or what to display in an overlay via customization options or just setting variables `go-if-err-nil-regexp-alist` and `go-if-err-nil-face`.
+You can customize a face for overlays or what to display in an overlay via customization options or just setting variables `go-if-err-nil-regexp-alist` and `go-prettify-face`.
 
 How the configuration is looked in my `init.el` with `use-package`:
 ``` emacs-lisp
-(use-package go-if-err-nil
+(use-package go-prettify
   :after (go-mode)
   :config
-  (define-key go-mode-map (kbd "C-c C-e") #'go-if-err-nil-mode))
+  (define-key go-mode-map (kbd "C-c C-e") #'go-prettify-mode))
+
+(use-package go-mode
+  :hook
+  (go-mode . (lambda () (go-prettify-mode 1))))
 ```
 
--- go-prettify-mode.el
+++ go-prettify-mode.el
@@ -1,11 +1,11 @@
-;;; go-if-err-nil.el --- Hide `if err != nil' statements in Go in an informative way. -*- lexical-binding: t -*-
+;;; go-prettify-mode.el --- Hide `if err != nil' and other statements in Go in an informative way. -*- lexical-binding: t -*-
 
-;; Author: Gleb Zakharov <snyssfx@gmail.com>
+;; Author: Gleb Zakharov <snyssfx@posteo.net>
 ;; Version: 1.0
 ;; Keywords: languages go tools
-;; URL: https://git.sr.ht/~snyssfx/go-if-err-nil.el
+;; URL: https://git.sr.ht/~snyssfx/go-prettify-mode.el
 
-;;; Copyright © 2023 Gleb Zakharov <snyssfx@gmail.com>
+;;; Copyright © 2023 Gleb Zakharov <snyssfx@posteo.net>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -24,10 +24,24 @@
 
 ;;; Code:
 
-(defgroup go-if-err-nil nil
-  "Hide `if err != nil' statements in Go programs."
-  :prefix "go-if-err-nil-"
+(defgroup go-prettify-mode nil
+  "Hide `if err != nil' and other statements in Go programs."
+  :prefix "go-prettify-mode-"
   :group 'convenience)
+
+(defcustom go-prettify-feature-list
+  '('if-err-nil
+    '1-code-block
+    'range
+    'lambda-func)
+  "What features turn on in the package"
+  :type '(list string)
+  :group 'go-prettify-mode)
+
+(defface go-prettify-face
+  '((t :inherit font-lock-comment-face))
+  "Face of an overlay for `if err != nil' statement."
+  :group 'go-prettify-mode)
 
 (defcustom go-if-err-nil-regexp-alist
   '(("^[ \t\n\r]*" "")
@@ -47,22 +61,47 @@
     ("\"" ""))
   "Alist of pairs of regexps to their replaces for every line inside if err != nil code blocks."
   :type '(alist :key-type regexp :value-type string)
-  :group 'go-if-err-nil)
+  :group 'go-prettify-mode)
+
+(make-variable-buffer-local
+ (defvar go-prettify--overlays '()
+   "Private local variable, overlays of the buffer."))
+
+(defconst go-prettify--invisible-symbol
+  'go-prettify--invisible-symbol
+  "Symbol to add in invisible property of overlays.")
 
 (defcustom go-if-err-nil--err-regexp
   "\\(;\\|if\\) \\(err != nil\\|!ok\\) {"
   "A variable of what regexp should be hidden in Go code."
   :type 'regexp
-  :group 'go-if-err-nil)
+  :group 'go-prettify-mode)
 
-(defface go-if-err-nil-face
-  '((t :inherit font-lock-comment-face))
-  "Face of an overlay for `if err != nil' statement."
-  :group 'go-if-err-nil)
+(defun go-prettify--make-overlay (buffer beginning end replace-to-str)
+  "Creates overlay with all properties between BEGINNING and END."
+  (let ((overlay (make-overlay beginning end)))
+    (put-text-property
+     0 (length replace-to-str)
+     'face 'go-prettify-face
+     replace-to-str)
+    (overlay-put overlay
+                 'invisible
+                 go-if-err-nil--invisible-symbol)
+    (overlay-put overlay
+                 'after-string
+                 replace-to-str)
+    overlay))
 
-(make-variable-buffer-local
- (defvar go-if-err-nil--overlays '()
-   "Private local variable, overlays of the buffer."))
+(defun go-prettify--get-or-make-overlay (buffer beginning end str-replace-to)
+  (let* ((ov
+          (cl-find-if
+           (lambda (ov) (eq
+                         go-prettify--invisible-symbol
+                         (overlay-get ov 'invisible)))
+           (overlays-at beginning))))
+    (or ov
+        (go-prettify--make-overlay
+         buffer beginning end str-replace-to))))
 
 (defun go-if-err-nil--replace-line-in-overlay (line)
   "Apply all regexp to replacements from alist to the line."
@@ -89,23 +128,6 @@
      (string-join lines-replaced)
      "; ")))
 
-(defun go-if-err-nil--make-overlay (buffer beginning end)
-  "Creates overlay with all properties between BEGINNING and END."
-  (let ((overlay (make-overlay beginning end))
-        (str (go-if-err-nil--string-after-overlay
-              buffer beginning end)))
-    (put-text-property
-     0 (length str)
-     'face 'go-if-err-nil-face
-     str)
-    (overlay-put overlay
-                 'invisible
-                 'go-if-err-nil--invisible-symbol)
-    (overlay-put overlay
-                 'after-string
-                 str)
-    overlay))
-
 (defun go-if-err-nil--make-overlay-at-point (buffer)
   "Creates an overlay and stores it for the future use."
   (let* ((beginning (progn
@@ -116,32 +138,38 @@
                 (backward-char)
                 (forward-sexp)
                 (point))))
-    (setq go-if-err-nil--overlays
+    (setq go-prettify--overlays
           (cons
-           (go-if-err-nil--make-overlay buffer beginning end)
-           go-if-err-nil--overlays))))
+           (go-prettify--get-or-make-overlay
+            buffer
+            beginning end
+            (go-if-err-nil--string-after-overlay
+             buffer beginning end))
+           go-prettify--overlays))))
 
-(defun go-if-err-nil-turn-on (buffer)
+(defun go-prettify-turn-on (buffer)
   "Searches for every `err != nil' in the buffer and creates overlays for them."
   (interactive (list (current-buffer)))
-  (add-to-invisibility-spec 'go-if-err-nil--invisible-symbol)
+  (add-to-invisibility-spec go-if-err-nil--invisible-symbol)
   (save-excursion
     (with-current-buffer buffer
       (save-restriction
         (widen)
         (goto-char (point-min))
         (while (search-forward-regexp go-if-err-nil--err-regexp nil t 1)
-          (go-if-err-nil--make-overlay-at-point buffer))))))
+          (if (string-search "//" (thing-at-point 'line 'no-properties))
+              (end-of-line)
+            (go-if-err-nil--make-overlay-at-point buffer)))))))
 
-(defun go-if-err-nil-turn-off (buffer)
+(defun go-prettify-turn-off (buffer)
   "Removes old overlays from the buffer."
   (interactive (list (current-buffer)))
-  (remove-from-invisibility-spec 'go-if-err-nil--invisible-symbol)
-  (mapcar #'delete-overlay go-if-err-nil--overlays)
-  (setq go-if-err-nil--overlays nil))
+  (remove-from-invisibility-spec go-prettify--invisible-symbol)
+  (mapcar #'delete-overlay go-prettify--overlays)
+  (setq go-prettify--overlays nil))
 
 ;;;###autoload
-(define-minor-mode go-if-err-nil-mode
+(define-minor-mode go-prettify-mode
   "Minor mode that adds overlays to `if err != nil' statements.
 
 To turn it on in every Go buffer, add a hook:
@@ -150,11 +178,11 @@ To turn it on in every Go buffer, add a hook:
 To toggle it via a hotkey add this code:
     (define-key go-mode-map (kbd \"C-c C-e\") #'go-if-err-nil-mode)
    "
-  :group 'go-if-err-nil
-  (if go-if-err-nil-mode
-      (go-if-err-nil-turn-on (current-buffer))
-    (go-if-err-nil-turn-off (current-buffer))))
+  :group 'go-prettify-mode
+  (if go-prettify-mode
+      (go-prettify-turn-on (current-buffer))
+    (go-prettify-turn-off (current-buffer))))
 
-(provide 'go-if-err-nil)
+(provide 'go-prettify)
 
-;;; go-if-err-nil.el ends here
+;;; go-prettify-mode.el ends here
